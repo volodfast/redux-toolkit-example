97f55a6884cd6502ac9c1d0633289e7212afc8bb
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-21 23:58:25 +0200

    Load episode list data with RTK query

--- src/modules/episode/components/EpisodeList/EpisodeList.tsx
+++ src/modules/episode/components/EpisodeList/EpisodeList.tsx
@@ -1,9 +1,21 @@
 import { FC } from 'react';
+// components
+import EpisodeListItem from 'modules/episode/components/EpisodeListItem';
+// hooks
+import { useGetEpisodeListQuery } from 'modules/episode/episode.store';
 
 const EpisodeList: FC = () => {
+  const { data, isLoading } = useGetEpisodeListQuery('');
+
   return (
     <div>
       <h2>Episode list</h2>
+
+      {isLoading && <div>Loading...</div>}
+
+      {data?.results?.map((episode) => {
+        return <EpisodeListItem key={episode.id} episode={episode} />;
+      })}
     </div>
   );
 };
--- src/modules/episode/episode.store.ts
+++ src/modules/episode/episode.store.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { ApiListResponse } from 'api/api.interfaces';
 // interfaces
 import { Episode } from './episode.interface';
 
@@ -6,7 +7,7 @@ export const episodeApi = createApi({
   reducerPath: 'episodeApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/' }),
   endpoints: (builder) => ({
-    getEpisodeList: builder.query({
+    getEpisodeList: builder.query<ApiListResponse<Episode[]>, unknown>({
       query: () => `episode`,
     }),
   }),
